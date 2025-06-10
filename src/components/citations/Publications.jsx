d6147352c4ff15f1516b36ad98d765ee7817f9d6
Derek Evans <derekevans@Dereks-MacBook-Air.local>
2025-06-10 07:05:14 -0500

    adding pub link

--- src/components/citations/Publications.jsx
+++ src/components/citations/Publications.jsx
@@ -7,15 +7,9 @@ export default class Publications extends React.Component {
     render() {
         return <React.Fragment>
             <h2>Publications</h2>
-
-	        { 
-                this.PUBLICATIONS.map((pub, idx) => {
-                    return <p key={idx}>Evans, D.A., K.W.J. Williard, and J.E. Schoonover. (2016). Comparison of terrain indices and landform classification procedures in low-relief agricultural fields. Journal of Geospatial Applications in Natural Resources, 1, 1-17. Available at: 
-                        <a href="https://scholarworks.sfasu.edu/j_of_geospatial_applications_in_natural_resources/vol1/iss1/1/">https://scholarworks.sfasu.edu/j_of_geospatial_applications_in_natural_resources/vol1/iss1/1/</a>.
-                    </p>
-                })
-            }
-
+	        <p>Evans, D.A., K.W.J. Williard, and J.E. Schoonover. (2016). Comparison of terrain indices and landform classification procedures in low-relief agricultural fields. Journal of Geospatial Applications in Natural Resources, 1, 1-17. Available at: 
+                <a href="https://scholarworks.sfasu.edu/j_of_geospatial_applications_in_natural_resources/vol1/iss1/1/">https://scholarworks.sfasu.edu/j_of_geospatial_applications_in_natural_resources/vol1/iss1/1/</a>.
+            </p>
         </React.Fragment>
     }
 }
